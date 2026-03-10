59f1176fdb6412e3a0c49ea9edd8b23b69462766
asonkes <audrey.sonkes@gmail.com>
2026-03-10 16:03:45 +0100

    version production

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,16 @@ const express = require("express");
 //création du serveur express
 const server = express();
 
-const { PORT } = process.env;
+// Partie deve(pas production => vercel)
+// const { PORT } = process.env;
 
 // Sert à permettre à mon frontend à faire des requêtes ur le backend
 const cors = require("cors");
-server.use(cors({ origin: "http://localhost:5173" }));
+// Partie developpement
+// server.use(cors({ origin: "http://localhost:5173" }));
+
+// Partie production(vercel)
+server.use(cors());
 
 // Permet avec ce code d'envoyer du json(pour les requêtes 'post' par exemple)
 server.use(express.json());
@@ -20,6 +25,14 @@ const router = require("./routes");
 server.use("/api", router);
 
 // Écouter le serveur sur un port spécifique
-server.listen(PORT, () => {
-  console.log(`🚀 Express Server started on port ${PORT}`);
-});
+// Partie (production => vercel)
+// server.listen(PORT, () => {
+//   console.log(`🚀 Express Server started on port ${PORT}`);
+// });
+
+if (process.env.NODE_ENV !== "production") {
+  const { PORT } = process.env;
+  server.listen(PORT, () => {
+    console.log(`🚀 Express Server started on port ${PORT}`);
+  });
+}
