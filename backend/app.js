a103e144ec8abe4d0ab787429434c067865c4073
asonkes <audrey.sonkes@gmail.com>
2026-03-10 16:58:44 +0100

    production

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,37 @@
-//import d'express
-const express = require("express");
+// //import d'express
+// const express = require("express");
 
-//création du serveur express
-const server = express();
+// //création du serveur express
+// const server = express();
 
-// Partie deve(pas production => vercel)
 // const { PORT } = process.env;
 
-// Sert à permettre à mon frontend à faire des requêtes ur le backend
-const cors = require("cors");
-// Partie developpement
+// // Sert à permettre à mon frontend à faire des requêtes ur le backend
+// const cors = require("cors");
 // server.use(cors({ origin: "http://localhost:5173" }));
 
-// Partie production(vercel)
-server.use(cors());
+// // Permet avec ce code d'envoyer du json(pour les requêtes 'post' par exemple)
+// server.use(express.json());
 
-// Permet avec ce code d'envoyer du json(pour les requêtes 'post' par exemple)
-server.use(express.json());
+// // On importe le router
+// const router = require("./routes");
 
-// On importe le router
-const router = require("./routes");
+// // Déclarer qu'on utilise ce router sur le serveur
+// server.use("/api", router);
 
-// Déclarer qu'on utilise ce router sur le serveur
-server.use("/api", router);
-
-// Écouter le serveur sur un port spécifique
-// Partie (production => vercel)
+// // Écouter le serveur sur un port spécifique
 // server.listen(PORT, () => {
 //   console.log(`🚀 Express Server started on port ${PORT}`);
 // });
 
-if (process.env.NODE_ENV !== "production") {
-  const { PORT } = process.env;
-  server.listen(PORT, () => {
-    console.log(`🚀 Express Server started on port ${PORT}`);
-  });
-}
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
 
-// pour production(vercel)
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use("/api", router);
+
 module.exports = server;
