e97cc84afc2dd4f8eeaa4b832bb6f4c3b2f6ad31
Audrey Sonkes <a.sonkes@interface3.be>
2026-03-11 13:29:03 +0100

    production

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,12 @@
 
 const router = require("express").Router();
 
+// router.get("/", (req, res) => {
+//   res.send("Bienvenue sur le router de l'API de cet exercice !", 200);
+// });
+
 router.get("/", (req, res) => {
-  res.send("Bienvenue sur le router de l'API de cet exercice !", 200);
+  res.status(200).send("Bienvenue sur le router de l'API de cet exercice !");
 });
 
 // import du router message
