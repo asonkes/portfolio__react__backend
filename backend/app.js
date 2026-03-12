f507101f4d05a4b8803b9cd1abf792f6fcea407a
asonkes <audrey.sonkes@gmail.com>
2026-03-12 10:06:38 +0100

    prod

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
+//import d'express
 const express = require("express");
-const cors = require("cors");
-const router = require("./routes");
 
+//création du serveur express
 const server = express();
 
-server.use(cors());
-server.options("*", cors());
+// const { PORT } = process.env;
 
+// Sert à permettre à mon frontend à faire des requêtes ur le backend
+const cors = require("cors");
+server.use(cors());
+
+// Permet avec ce code d'envoyer du json(pour les requêtes 'post' par exemple)
 server.use(express.json());
+
+// On importe le router
+const router = require("./routes");
+
+// Déclarer qu'on utilise ce router sur le serveur
 server.use("/api", router);
+
+// // Écouter le serveur sur un port spécifique
+// server.listen(PORT, () => {
+//   console.log(`🚀 Express Server started on port ${PORT}`);
+// });
 
 module.exports = server;
